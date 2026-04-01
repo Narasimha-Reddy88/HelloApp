dc8ba16b053e1fa9160b97f6b6feb19ef08fac01
Narasimha-Reddy88 <narasimhareddyveera94@gmail.com>
2026-04-01 17:13:24 +0530

    first commit

---
+++ HelloApp4.java
@@ -0,0 +1,18 @@
+public class HelloApp4 {
+    public static void main(String[] args) {
+
+        StringBuilder nameBuilder = new StringBuilder();
+
+        for (int i = 0; i < args.length; i++) {
+            nameBuilder.append(args[i]);
+
+            if (i < args.length - 1) {
+                nameBuilder.append(", ");
+            }
+        }
+
+        String name = nameBuilder.toString();
+
+        System.out.println("Hello, " + name + "!");
+    }
+}
